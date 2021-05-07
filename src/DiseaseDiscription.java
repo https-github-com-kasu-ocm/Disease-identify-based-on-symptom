001c5dc332db43462538f7ac7e6705c25630888b
kasu8424 <kasuhailu120@gmail.com>
2021-05-07 15:14:06 -0700

    Simple Drug Reaction is Added..

--- src/DiseaseDiscription.java
+++ src/DiseaseDiscription.java
@@ -8,7 +8,7 @@ public class DiseaseDiscription {
 		String d="";
 	HashMap<String, String> discribe=new HashMap<String, String>();
 	discribe.put("Fungal infection", "Some fungi reproduce through tiny spores in the air. \nYou can inhale the spores or they can land on you. \nAs a result, fungal infections often start in the lungs or on the skin.\n You are more likely to get a fungal infection if you have a \nweakened immune system or take antibiotics.");
-	 
+	discribe.put("Drug Reaction","A drug allergy is an allergic reaction \nto a medication. With an allergic reaction, \nyour immune system, which fights \ninfection and disease, reacts to the drug. \nThis reaction can cause symptoms such as rash, fever, and trouble breathing. \nTrue drug allergy is not common.");
 	
 	
 	for (Map.Entry<String,String> entry : discribe.entrySet())
